1bae2ba89c24ef484777ab95d0989049dbc66750
methlouthi syrine <methlouthisyrine@methlouthis-mac.local>
2019-03-05 12:29:58 -0800

    commit applyLeaves (1)

---
+++ CuteBrains/Controlers/Leaves/ApplyLeaveViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ApplyLeaveViewController.swift
+//  CuteBrains
+//
+//  Created by methlouthi syrine on 3/5/19.
+//  Copyright © 2019 fahmi. All rights reserved.
+//
+
+import UIKit
+
+class ApplyLeaveViewController: UIViewController {
+
+    @IBOutlet weak var FromTextField: UITextField!
+    
+    @IBOutlet weak var ToTextField: UITextField!
+    
+    @IBOutlet weak var IsHalfDayPickerView: UIPickerView!
+    
+    @IBOutlet weak var LeaveTypePickerView: UIPickerView!
+    
+    @IBOutlet weak var ReasonTextField: UITextField!
+    
+    @IBAction func applyLeaveButton(_ sender: Any) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
